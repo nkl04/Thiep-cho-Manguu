abc13ab77afb61ee4a7c676a75fc1127dc6901c3
Nguyễn Kim Long <131468660+nkl04@users.noreply.github.com>
2025-01-28 23:38:42 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '\nManguu';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Năm Mới gửi tới';
+const titleCard = 'Tặng Phan Thị Xuân Mai đáng yêu nhất cuộc đời Long';
+const contentCard = 'Chúc mừng năm mới. Chúc mọi điều tốt đẹp luôn đến với em. Mong công chúa của anh sẽ luôn vui vẻ, hạnh phúc, thành công trong cuộc sống và rực rỡ trên con đường đầy hoa của chúng ta. Một năm nữa lại qua, và chúng ta sẽ bước thêm được 1 bước nữa. Anh yêu em nhiều lắm Mai ạ. Mong em cũng sẽ luôn yêu anh như anh yêu em. Chúc cho gia đình em luôn được mạnh khỏe và may mắn nhé Mai. Yêu em nhiều ';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
